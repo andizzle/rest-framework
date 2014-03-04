45035569a839f819ed472df46c582b671beb5714
Andy Zhang <andizzle.zhang@gmail.com>
2014-03-04 15:25:56 +1100

    Add HyperlinkedJSONSerializer.

---
+++ src/Andizzle/Rest/Serializers/HyperlinkedJSONSerializer.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Andizzle\Rest\Serializers;
+
+use REST;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Contracts\ArrayableInterface;
+
+class HyperlinkedJSONSerializer extends BaseSerializer {
+
+    protected $api_prefix = '';
+
+    public function __construct() {
+
+        $this->sideload_limit = Config::get('api.sideloads_limit');
+        $this->api_prefix = $this->getApiPrefix();
+
+    }
+
+    /**
+     * Serialize instance to json ready array.
+     *
+     * @param \Illuminate\Support\Contracts\ArrayableInterface $instance
+     * @param boolean $withRelations
+     * @return array
+     */
+    public function serialize(ArrayableInterface $instance, $root, $withRelations = true, $limit = null) {
+
+        $relationship = array();
+
+        if( $limit )
+            $this->sideload_limit = $limit;
+
+        $serialized_data = parent::serialize($instance, $root, $withRelations);
+        $root = $this->getRoot($instance, $root);
+
+        if( $withRelations ) {
+            $serialized_data[$root] = $this->serializeKeys($instance)->toArray();
+        }
+
+        return array_merge($serialized_data, $relationship);
+
+    }
+
+    /**
+     * Serialize all toManys to keys
+     *
+     * @param ArrayableInterface $instance
+     * @return $instance
+     */
+    public function serializeKeys(ArrayableInterface $instance) {
+
+        $is_collection = True;
+        $result = new Collection;
+        if( !$this->isCollection($instance) ) {
+            $is_collection = False;
+            $collection = new Collection;
+            $instance = $collection->add($instance);
+        }
+
+        $instance->transform(function($item)
+        {
+
+            $links = array();
+            $side_loads = $item->getSideLoads();
+
+            foreach($side_loads as $load) {
+
+                if($item->{$load} instanceof Collection) {
+                    // we build link to ids of a model.
+                    // e.g, /api/v1/books?ids=1,2,3
+                    $value = $item->{$load}->take($this->sideload_limit);
+
+                    if($this->isEmptyOrNull($value)) {
+                        $item->__unset($load);
+                        continue;
+                    }
+
+                    $root = $value->first()->getRoot();
+                    $pk_field = str_plural($value->first()->getKeyName());
+                    $ids = $value->modelKeys();
+
+                } else {
+                    // otherwise the result is an id. e.g: 2
+                    if( $value = $item->{$load} ) {
+                        $root = $value->getRoot();
+                        $pk_field = $value->getKeyName();
+                        $ids = array($value->getKey());
+                    }
+
+                }
+
+                $item->__unset($load);
+                $links[$root] = $this->buildLink($root, $pk_field, $ids);
+
+            }
+
+            if( count($links) )
+                $item->setAttribute('links', $links);
+            return $item;
+
+        });
+
+        if( !$is_collection )
+            return $instance->pop();
+
+        return $instance;
+
+    }
+
+    public function buildLink($root, $pk_field, $ids) {
+
+        return $this->api_prefix . '/' . $root . '?' . $pk_field . '=' . implode(',', $ids);
+
+    }
+
+
+    /**
+     * Figure out the api prefix base on incoming request.
+     *
+     * @return string
+     */
+    public function getApiPrefix() {
+        $prefix = '';
+        $api_versions = Config::get('api.deprecated');
+        array_push($api_versions, Config::get('api.version'));
+
+        $segments = Request::segments();
+        foreach( $segments as $segment ) {
+
+            $prefix .= '/' . $segment;
+            if( in_array($segment, $api_versions) )
+                break;
+
+        }
+
+        return $prefix;
+
+    }
+
+
+}
