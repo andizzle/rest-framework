0d8c23c8e59ca3c9f7fc51298e549918b9cc0c1b
Andy Zhang <andizzle.zhang@gmail.com>
2015-05-08 08:49:50 +1000

    merge all convert case to one function

--- src/RestServer.php
+++ src/RestServer.php
@@ -66,76 +66,36 @@ class RestServer {
 
     }
 
-    /**
-     * Snakecase an array's keys.
-     *
-     * @param array $input
-     * @return array
-     */
-    private function snakeCaseInput(array $input) {
+    private function convertInput(array &$input, $case) {
 
-        $convertedInput = array();
         foreach($input as $key => $value) {
 
-            if(is_array($value)) {
-                // Recursively run to loop through all keys
-                $value = $this->snakeCaseInput($value);
+            if(is_array($value))
+                $this->convertInput($value, $case);
+
+            $converted_key = $key;
+            switch($case) {
+            case 'snakeCase':
+                $converted_key = snake_case($key);
+                break;
+            case 'camelCase':
+                $converted_key = camel_case($key);
+                break;
+            case 'studlyCase':
+                $converted_key = studly_case($key);
+                break;
+            default:
+                break;
             }
 
-            $convertedInput[snake_case($key)] = $value;
+            if($key !== $converted_key) {
+                $input[$converted_key] = $value;
+                unset($input[$key]);
+            }
 
         }
 
-        return $convertedInput;
-
-    }
-
-    /**
-     * Camelcase an array's keys.
-     *
-     * @param array $input
-     * @return array
-     */
-    private function camelCaseInput(array $input) {
-
-        $convertedInput = array();
-        foreach($input as $key => $value) {
-
-            if(is_array($value)) {
-                // Recursively run to loop through all keys
-                $value = $this->camelCaseInput($value);
-            }
-
-            $convertedInput[camel_case($key)] = $value;
-
-        }
-
-        return $convertedInput;
-
-    }
-
-
-    /**
-     * Studlycase an array's keys.
-     *
-     * @param array $input
-     * @return array
-     */
-    private function studlyCaseInput(array $input) {
-
-        $convertedInput = array();
-        foreach($input as $key => $value) {
-
-            if(is_array($value)) {
-                // Recursively run to loop through all keys
-                $value = $this->studlyCaseInput($value);
-            }
-
-            $convertedInput[studly_case($key)] = $value;
-
-        }
-
-        return $convertedInput;
+        return $input;
 
     }
 
